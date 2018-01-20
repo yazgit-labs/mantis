63f414facef8526f07a8f4bd27a54d6d47513adc
Emir Cem Gezer <emircem.gezer@gmail.com>
2018-01-21 02:16:18 +0300

    changed directory system, hasn\'t tested yet

---
+++ kezbot/scripts/setup.bash
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+sudo cp $(rospack find kezbot_udev)/udev/02-kezbot.rules /etc/udev/rules.d/
+ROBOT_NAME=""
+read -e -p "Enter robot name (no '-' is allowed): " -i "kezbot" ROBOT_NAME
+sudo echo "#!/bin/bash
+export ROBOT_NAME=$ROBOT_NAME
+" > /etc/profile.d/kezbot.sh
+
+if [ "$?" -ne 0 ] ; then
+  printf "%s\n" "${red}Env variables has not set." >&2
+else
+  printf "%s\n" "${green}Success.."
+fi
